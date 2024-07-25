01f6a25f35c8079e508a641d60de5b96f6e1ef24
María Fernanda González <mafergonzalezw@gmail.com>
2024-07-25 01:18:18 +0000

    Funcional

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio];
+  }
+
+  let numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let palo = ["♦", "♠", "♣", "♥"];
+
+  let paloCarta = aleatorio(palo);
+  let elementPaloCarta = document.querySelector("#paloCarta");
+  elementPaloCarta.innerHTML = paloCarta;
+
+  let numCarta = aleatorio(numero);
+  let elementnumCarta = document.querySelector("#numCarta");
+  elementnumCarta.innerHTML = numCarta;
+
+  let elementPaloCartaAbajo = document.querySelector("#paloCartaAbajo");
+  elementPaloCartaAbajo.innerHTML = paloCarta;
+
+  elementPaloCartaAbajo.style.transform = "rotate(180deg)";
+  if (paloCarta == "♥" || paloCarta == "♦") {
+    elementPaloCarta.style.color = "red";
+    elementPaloCartaAbajo.style.color = "red";
+  }
 };
